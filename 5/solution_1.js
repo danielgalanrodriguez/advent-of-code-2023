904cb285290096e11ec4ef0c9763e0a9a9aac60d
Daniel Galán <danielgalanrodri@gmail.com>
2023-12-06 21:11:24 +0100

    format code

--- 5/solution_1.js
+++ 5/solution_1.js
@@ -1,46 +1,40 @@
 // problem_1.txt
 
 const readFile = require("../readFile");
-const seedsData = {}
+const seedsData = {};
 
 function identifyStep(line) {
-  console.log('line: ', line);
-  const step = line.split(' ')[0]
-  console.log('step: ', step, /seeds/.test(step));
+  console.log("line: ", line);
+  const step = line.split(" ")[0];
+  console.log("step: ", step, /seeds/.test(step));
 
-  if (! /:/.test(step)) { console.log('number line'); return }
-  if (/seeds/.test(step)) { saveSeedsNumbers(line.split(':')[1]); return }
-  if( /seed-to-soil*/.test(step)) translateSeedToSoil(line.split(':')[1])
+  if (!/:/.test(step)) {
+    console.log("number line");
+    return;
+  }
+  if (/seeds/.test(step)) {
+    saveSeedsNumbers(line.split(":")[1]);
+    return;
+  }
+  if (/seed-to-soil*/.test(step)) translateSeedToSoil(line.split(":")[1]);
 }
 
 function saveSeedsNumbers(data) {
-  const seedNumbers = data.trim().split(' ').filter(Boolean)
+  const seedNumbers = data.trim().split(" ").filter(Boolean);
 
-  seedNumbers.forEach(seedNumber => {
-    seedsData[seedNumber] = {}
+  seedNumbers.forEach((seedNumber) => {
+    seedsData[seedNumber] = {};
   });
-
 }
 
-function translateSeedToSoil(){}
+function translateSeedToSoil() {}
 
 function readLine(line) {
-  identifyStep(line)
+  identifyStep(line);
 }
-
 
 function onEnd() {
   console.log("Finished reading the file. Solution:", seedsData);
 }
 
-
-
-readFile('test_data.txt', readLine, onEnd)
-
-
-
-
-
-
-
-
+readFile("test_data.txt", readLine, onEnd);
